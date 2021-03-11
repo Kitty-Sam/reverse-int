5dc162e2c662c2d869d1898777fd0c14f6a3337a
kitty-sam <www.moonlight@mail.ru>
2021-03-11 22:15:50 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (a) {
+
+    if (a>=0) {
+      let b=Number(a.toString().split("").reverse().join(""));
+      return b;
+    } else if (a<0){
+      let c=Number(Math.abs(a).toString().split("").reverse().join(""));
+      return c;
   
-}
+    }
+   
+  }
